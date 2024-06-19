5e076918a4767fc9fdf55ba86a45ccc286e33f28
Evgeniy <evgnec@gmail.com>
2024-06-19 20:09:20 +0300

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';  
+import lodash, { times } from 'lodash';
+  
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+player.on('timeupdate', lodash.debounce(function () {
+        console.log('played the video!');
+    }), 1);
 
