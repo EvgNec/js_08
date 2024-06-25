12ff91aa8b5f5f5167d81a46ddf2b17c20963974
EvgNec <evgnec@gmail.com>
2024-06-25 21:03:41 +0000

    Deploying to gh-pages from @ EvgNec/js_08@59c5c3bd2fd7db9671597676b1fb2bf68ee02421 🚀

--- 04-to_do.e407dc31.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.getElementById("myInput");var t=document.getElementById("addBtn");document.getElementById("myUL");t.addEventListener("click",(function(){var t=e.value.trim();console.log("task",t)}))}();
-//# sourceMappingURL=04-to_do.e407dc31.js.map
