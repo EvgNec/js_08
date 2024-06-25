b8fa975a8e1d9efd68489d8d8246247664f13f53
Evgeniy <evgnec@gmail.com>
2024-06-26 00:34:31 +0300

    upd

--- src/js/04-functions.js
+++ src/js/04-functions.js
@@ -10,15 +10,29 @@ function addNewTask() {
   }
   createLi(task);
 }
-
 function createLi(text) {
   const liEl = document.createElement('li');
   liEl.textContent = text;
-  myUL.appendChild(liEl);
+    myUL.appendChild(liEl);
+    addCloseButton(liEl);
 }
 // function createLi(text) {
 //    const liEl = `<li>${text}</li>`
 //     myUL.insertAdjacentHTML("beforeend", liEl);
 // }
 
-export { addNewTask };
+function addCloseButton(target) { 
+    const span = document.createElement('span');
+    const txt = document.createTextNode("\u00D7");
+    span.className = 'close';
+    span.appendChild(txt); 
+    target.appendChild(span);
+}
+
+function handlTaskBehavior({ target}) {
+    console.log("target", target)
+    
+}
+
+
+export { addNewTask, handlTaskBehavior };
--- src/js/04-to_do.js
+++ src/js/04-to_do.js
@@ -1,9 +1,11 @@
-import { addNewTask } from './04-functions';
+import { addNewTask, handlTaskBehavior } from './04-functions';
 
+const myUL = document.getElementById('myUL');
 const addbtn = document.getElementById("addBtn");
 
 
 addbtn.addEventListener("click", addNewTask);
+myUL.addEventListener("click", handlTaskBehavior);
 
 
 
