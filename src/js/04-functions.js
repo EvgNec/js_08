59c5c3bd2fd7db9671597676b1fb2bf68ee02421
Evgeniy <evgnec@gmail.com>
2024-06-26 00:03:14 +0300

    upd

--- src/js/04-functions.js
+++ src/js/04-functions.js
@@ -1,8 +1,24 @@
-const myInput = document.getElementById("myInput");
+const myInput = document.getElementById('myInput');
+const myUL = document.getElementById('myUL');
 
 function addNewTask() {
-    const task = myInput.value.trim();
-    console.log("task", task)
+  const task = myInput.value.trim();
+  myInput.value = '';
+  if (task === '') {
+    alert('Please enter text');
+    return;
+  }
+  createLi(task);
 }
+
+function createLi(text) {
+  const liEl = document.createElement('li');
+  liEl.textContent = text;
+  myUL.appendChild(liEl);
+}
+// function createLi(text) {
+//    const liEl = `<li>${text}</li>`
+//     myUL.insertAdjacentHTML("beforeend", liEl);
+// }
 
 export { addNewTask };
--- src/js/04-to_do.js
+++ src/js/04-to_do.js
@@ -1,16 +1,11 @@
 import { addNewTask } from './04-functions';
 
 const addbtn = document.getElementById("addBtn");
-const myUL = document.getElementById("myUL");
+
 
 addbtn.addEventListener("click", addNewTask);
 
-// function addToDo(e) {
-//  console.log("e.target.value", e.target)   
-//    const elements = `<li>${e.target.value}</li>`
-//     list.insertAdjacentHTML("beforeend", elements);
 
-// }
 
 
 
