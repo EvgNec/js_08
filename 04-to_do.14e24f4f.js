c53cad00029cae23e6c6d0a6363fc5b834948a8d
EvgNec <evgnec@gmail.com>
2024-06-25 21:34:51 +0000

    Deploying to gh-pages from @ EvgNec/js_08@b8fa975a8e1d9efd68489d8d8246247664f13f53 🚀

--- 04-to_do.14e24f4f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.getElementById("myInput"),t=document.getElementById("myUL");document.getElementById("addBtn").addEventListener("click",(function(){var n,d,l=e.value.trim();e.value="",""!==l?(n=l,(d=document.createElement("li")).textContent=n,t.appendChild(d)):alert("Please enter text")}))}();
-//# sourceMappingURL=04-to_do.14e24f4f.js.map
--- 04-to_do.1adf691d.js
+++
@@ -1,2 +0,0 @@
-const e=document.getElementById("myInput"),t=document.getElementById("myUL");document.getElementById("addBtn").addEventListener("click",(function(){const n=e.value.trim();e.value="",""!==n?function(e){const n=document.createElement("li");n.textContent=e,t.appendChild(n)}(n):alert("Please enter text")}));
-//# sourceMappingURL=04-to_do.1adf691d.js.map
