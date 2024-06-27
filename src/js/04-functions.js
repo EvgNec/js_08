ab0a87d497a5bebf7369c32444f94b6982c3a673
Evgeniy <evgnec@gmail.com>
2024-06-28 01:09:13 +0300

    upd

--- src/js/04-functions.js
+++ src/js/04-functions.js
@@ -18,12 +18,9 @@ function addNewTask() {
 }
 
 function createLi(text, isDone = false, id = currentID) {
-  console.log("currentID", currentID);
   const liEl = document.createElement('li');
   liEl.textContent = text; 
-  console.log("liEl.dataset.id1", liEl.dataset.id);
   liEl.dataset.id = id;
-  console.log("liEl.dataset.id2", liEl.dataset.id)
   if(isDone) liEl.classList.add("checked");
   myUL.appendChild(liEl);
   addCloseButton(liEl);
@@ -43,30 +40,32 @@ function addCloseButton(target) {
 
 function handlTaskBehavior({ target }) {
   const currentState = load(localStorageKey);
-  console.log("currentState", currentState)
+
 
   //  Target.tagName -повертає назву HTML-тега елемента Target
   // Target.nodeName -повертає імя пточного вузла у вигляді строки
   if (target.nodeName === 'LI') {
     target.classList.toggle('checked');
-    const taskIndex = currentState.findIndex(task => task.id = target.dataset.id);
-          console.log("taskIndex", taskIndex)
+    const taskIndex = currentState.findIndex((task) => Number(task.id) === Number(target.dataset.id));
+    currentState[taskIndex].isDone = !currentState[taskIndex].isDone;
   } else if (target.classList.contains('close')) {
     target.parentNode.remove();
+    const taskIndex = currentState.findIndex((task) => Number(task.id) === Number(target.parentNode.dataset.id));
+    currentState.splice(taskIndex, 1);
   }
+  save(localStorageKey, currentState);
 }
 
 function createTaskObj(text, isDone) {
   return {
     text,
     isDone,
-    id: 0,
+    id: currentID,
   };
 }
 
 function addTaskToLocalStorage(text, isDone = false) {
   const currentState = load(localStorageKey);
-  console.log('currentState', currentState);
   if (currentState === undefined) {
     //створюємо масив і додаємо туди перший об'єкт задачі
     const arr = [createTaskObj(text, isDone)];
