51fc292cab83fc4274a03435e6611b59f58fe1ab
Evgeniy <evgnec@gmail.com>
2024-06-28 20:04:08 +0300

    upd

---
+++ src/js/test/05-func.js
@@ -0,0 +1,7 @@
+function addCloseButton(target) {
+  const span = document.createElement('span');
+  const txt = document.createTextNode('\u00D7');
+  span.className = 'close';
+  span.appendChild(txt);
+  target.appendChild(span);
+}
