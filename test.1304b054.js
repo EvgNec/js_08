c8edf561ca55b4be89509e934d9d3bd6dd050f4a
EvgNec <evgnec@gmail.com>
2024-07-07 19:45:50 +0000

    Deploying to gh-pages from @ EvgNec/js_08@1714e117c65a35772ff571a36aabc428acd30e54 🚀

---
+++ test.1304b054.js
@@ -0,0 +1,2 @@
+const n=document.getElementById("addBtn"),t=document.getElementById("myUL");n.addEventListener("click",(function(){})),t.addEventListener("click",(function({target:n}){})),window.addEventListener("DOMContentLoaded",(function(){}));
+//# sourceMappingURL=test.1304b054.js.map
