b1e5426296388a484e5966eca876a38fcc6cdafc
EvgNec <evgnec@gmail.com>
2024-07-08 17:44:40 +0000

    Deploying to gh-pages from @ EvgNec/js_08@0fcfff1011844e406936b48c622ae6fedeb70c9e 🚀

--- test.1304b054.js
+++
@@ -1,2 +0,0 @@
-const n=document.getElementById("addBtn"),t=document.getElementById("myUL");n.addEventListener("click",(function(){})),t.addEventListener("click",(function({target:n}){})),window.addEventListener("DOMContentLoaded",(function(){}));
-//# sourceMappingURL=test.1304b054.js.map
--- test.5ddb15ae.js
+++
@@ -1,2 +0,0 @@
-!function(){var n=document.getElementById("addBtn"),t=document.getElementById("myUL");n.addEventListener("click",(function(){})),t.addEventListener("click",(function(n){n.target})),window.addEventListener("DOMContentLoaded",(function(){}))}();
-//# sourceMappingURL=test.5ddb15ae.js.map
