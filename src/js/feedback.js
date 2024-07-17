7848e6aab45482a7e3bce8ce91d614decacd61ce
Evgeniy <evgnec@gmail.com>
2024-07-17 18:18:34 +0300

    upd

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -3,24 +3,37 @@ const keyLS = 'feedback-form-state';
 const forms = document.querySelector(".feedback-form");
 // const forms = document.querySelector("#idd");
 const elMail = document.querySelector("input[name=email]");
-
-const elMes = document.querySelector("input[name=message]");
+const elMes = document.querySelector("textarea[name=message]");
 
 
 forms.addEventListener("input",  lodash.throttle(AddLS,500));
 forms.addEventListener("submit", onSubmit);
+const data = localStorage.getItem(keyLS);
+console.log("data", data)
+prevLS();
 
 function AddLS(e) {
     const tar = e.target.value;
-    console.log("dataset", e.target.dataset)
-    console.log("e.target", e.target)
-    console.log(tar);
-    console.log("elMail", elMail.value)
-    console.log("elMes", elMes)
+  const data = {
+  Mail: elMail.value,
+  Mes: elMes.value,
+    };
+    localStorage.setItem(keyLS, JSON.stringify(data));
+    // console.log(tar);
+    // console.log("elMail", elMail.value)
+    // console.log("elMes", elMes.value)
 
 }
 
-function setLocalStorageData(e) {
+function prevLS() {
+    if (data) {
+     console.log("e.target.value") 
+    } else {
+        console.log("Null") 
+}
+};
+
+function saveToLS(e) {
 
 };
 
