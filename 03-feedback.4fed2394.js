3e1e90b285259bc79ca5fee31fb6fb8152f3438d
EvgNec <evgnec@gmail.com>
2024-07-17 14:58:24 +0000

    Deploying to gh-pages from @ EvgNec/js_08@8fa188217428d9243ba2e29d267629d0bb343f4c 🚀

--- 03-feedback.4fed2394.js
+++
@@ -1,2 +0,0 @@
-!function(){console.log("0");var e=document.querySelector(".feedback-form");document.querySelector("input[name=email]"),document.querySelector("input[name=message]");e.addEventListener("input",(function(){console.log("1")}))}();
-//# sourceMappingURL=03-feedback.4fed2394.js.map
