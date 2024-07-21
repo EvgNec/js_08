08d7719338c3bac415e5192c578b5c4e0729c38d
Evgeniy <evgnec@gmail.com>
2024-07-21 16:48:35 +0300

    upd

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,41 +1,32 @@
 import lodash from 'lodash';
 const keyLS = 'feedback-form-state';
-const forms = document.querySelector(".feedback-form");
-// const forms = document.querySelector("#idd");
-const elMail = document.querySelector("input[name=email]");
-const elMes = document.querySelector("textarea[name=message]");
+const forms = document.querySelector('.feedback-form');
+const elMail = document.querySelector('input[name=email]');
+const elMes = document.querySelector('textarea[name=message]');
 
-
-forms.addEventListener("input",  lodash.throttle(AddLS,500));
-forms.addEventListener("submit", onSubmit);
+forms.addEventListener('input', lodash.throttle(AddLS, 500));
+forms.addEventListener('submit', onSubmit);
 const data = localStorage.getItem(keyLS);
-console.log("data", data)
 prevLS();
 
 function AddLS(e) {
-    const tar = e.target.value;
+  const tar = e.target.value;
   const data = {
-  Mail: elMail.value,
-  Mes: elMes.value,
-    };
-    localStorage.setItem(keyLS, JSON.stringify(data));
-    // console.log(tar);
-    // console.log("elMail", elMail.value)
-    // console.log("elMes", elMes.value)
-
+    Mail: elMail.value,
+    Mes: elMes.value,
+  };
+  localStorage.setItem(keyLS, JSON.stringify(data));
 }
 
 function prevLS() {
-    if (data) {
-     console.log("e.target.value") 
-    } else {
-        console.log("Null") 
+  if (data) {
+    const ls = JSON.parse(data);
+    console.log('ls', ls);
+    elMail.value = ls.Mail;
+    elMes.value = ls.Mes;
+  }
 }
-};
 
-function saveToLS(e) {
+function saveToLS(e) {}
 
-};
-
-function onSubmit(e) {
-}
+function onSubmit(e) {}
